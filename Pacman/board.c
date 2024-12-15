8f7c78e3f3d924a5cc1d00dd5ea14c2604a2ab6e
Michelangelo Ungolo <ungolomiki46@gmail.com>
2024-12-15 17:07:54 +0100

    created structure of pacman library.

--- Pacman/board.c
+++ Pacman/board.c
@@ -1,19 +1,15 @@
 /*********************************************************************************************************
 **--------------File Info---------------------------------------------------------------------------------
-** File name:           timer.h
-** Last modified Date:  2014-09-25
-** Last Version:        V1.00
-** Descriptions:        Prototypes of functions included in the lib_led, funct_led .c files
-** Correlated files:    lib_led.c, funct_led.c
+** It includes all functions for pacman board creation. 
 **--------------------------------------------------------------------------------------------------------       
 *********************************************************************************************************/
 
-/* lib_led */
-void LED_init(void);
-void LED_deinit(void);
+/* include libraries */ 
+#include "LPC17xx.h"
+#include "pacman.h"
 
-/* funct_led */
-void LED_On (unsigned int num);
-void LED_Off (unsigned int num);
-void LED_Out(unsigned int value);
+/* define constants */ 
+
+
+/* define functions */ 
 
--- Pacman/pacman.h
+++ Pacman/pacman.h
@@ -1,19 +1,6 @@
 /*********************************************************************************************************
 **--------------File Info---------------------------------------------------------------------------------
-** File name:           timer.h
-** Last modified Date:  2014-09-25
-** Last Version:        V1.00
-** Descriptions:        Prototypes of functions included in the lib_led, funct_led .c files
-** Correlated files:    lib_led.c, funct_led.c
+** Prototypes of functions defined in board.c, game.c 
+** It includes all functions for pacman game. 
 **--------------------------------------------------------------------------------------------------------       
 *********************************************************************************************************/
-
-/* lib_led */
-void LED_init(void);
-void LED_deinit(void);
-
-/* funct_led */
-void LED_On (unsigned int num);
-void LED_Off (unsigned int num);
-void LED_Out(unsigned int value);
-
